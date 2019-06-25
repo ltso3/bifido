f96b86c1315fac0496d2887eac1f0dfc07a66700
ltso <ltso@wellesley.edu>
2019-06-25 16:45:45 -0400

    updated entrez scrape

--- scripts/blast/entrez_scrape.py
+++ scripts/blast/entrez_scrape.py
@@ -13,21 +13,36 @@ with open('protein_hits.faa', 'w') as f:
                 query_id = record.query
                 for alignment in record.alignments:
                         hit_id = alignment.hit_id.split("|")[1]
-                        map_dict[query_id] = hit_id
                         try:
                                 seq = Entrez.efetch(db="protein", id=hit_id, rettype="fasta", retmode="text")
                         except HTTPError as err:
                                 time.sleep(20)
-                                if err.code == 400:
-                                        gis = Entrez.esearch(db="protein", term=hit_id, retmode="txt")
-                                        length = 0
-                                        for gi in gis['IdList']:
-                                                acc = Entrez.efetch(db="protein", id=gi, rettype="acc")
-                                                tmp = Entrez.efetch(db="protein", id=acc, rettype="fasta", retmode="text")
-                                                if len(tmp.read().split("\n", 1)[1]) > length:
-                                                        seq = Entrez.efetch(db="protein", id=acc, rettype="fasta", retmode="text")
-                                else:
-                                        seq = Entrez.efetch(db="protein", id=hit_id, rettype="fasta", retmode="text")
+                                try:
+                                        if err.code == 400:
+                                                gis = Entrez.esearch(db="protein", term=hit_id, retmode="txt")
+                                                for gi in Entrez.read(gis)['IdList']:
+                                                        acc = Entrez.efetch(db="protein", id=gi, rettype="acc")
+                                                        acc_str = acc.read().strip()
+                                                        tmp = Entrez.efetch(db="protein", id=acc_str, rettype="fasta", retmode="text")
+                                                        if len(tmp.read().split("\n", 1)[1]) > 100:
+                                                                seq = Entrez.efetch(db="protein", id=acc_str, rettype="fasta", retmode="text")
+                                                                hit_id = Entrez.efetch(db="protein", id=gi, rettype="acc").read().strip()
+                                        else:
+                                                seq = Entrez.efetch(db="protein", id=hit_id, rettype="fasta", retmode="text")
+                                except
+                                        time.sleep(200)
+                                        if err.code == 400:
+                                                gis = Entrez.esearch(db="protein", term=hit_id, retmode="txt")
+                                                for gi in Entrez.read(gis)['IdList']:
+                                                        acc = Entrez.efetch(db="protein", id=gi, rettype="acc")
+                                                        acc_str = acc.read().strip()
+                                                        tmp = Entrez.efetch(db="protein", id=acc_str, rettype="fasta", retmode="text")
+                                                        if len(tmp.read().split("\n", 1)[1]) > 100:
+                                                                seq = Entrez.efetch(db="protein", id=acc_str, rettype="fasta", retmode="text")
+                                                                hit_id = Entrez.efetch(db="protein", id=gi, rettype="acc").read().strip()
+                                        else:
+                                                seq = Entrez.efetch(db="protein", id=hit_id, rettype="fasta", retmode="text")
+                        map_dict[hit_id] = query_id
                         print(seq.read(), file=f, end='')
 
 f = open("protein_mapping.txt","w")
