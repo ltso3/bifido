f6742ad793f3bf4d95303bc2fe8f0dd6dd05da53
ltso <ltso@wellesley.edu>
2019-06-25 15:43:26 -0400

    updated entrez errors

--- scripts/blast/entrez_scrape.py
+++ scripts/blast/entrez_scrape.py
@@ -7,15 +7,29 @@ Entrez.email = "ltso@wellesley.edu"
 
 handle = open("/Users/laurentso/Desktop/repos/bifido/blast/output/blast_output_infantis_protein.xml")
 records = NCBIXML.parse(handle)
+map_dict = {}
 with open('protein_hits.faa', 'w') as f:
         for record in records:
                 query_id = record.query
                 for alignment in record.alignments:
                         hit_id = alignment.hit_id.split("|")[1]
+                        map_dict[query_id] = hit_id
                         try:
-                                time.sleep(20)
                                 seq = Entrez.efetch(db="protein", id=hit_id, rettype="fasta", retmode="text")
-                        except HTTPError:
+                        except HTTPError as err:
                                 time.sleep(20)
-                                seq = Entrez.efetch(db="protein", id=hit_id, rettype="fasta", retmode="text")
+                                if err.code == 400:
+                                        gis = Entrez.esearch(db="protein", term=hit_id, retmode="txt")
+                                        length = 0
+                                        for gi in gis['IdList']:
+                                                acc = Entrez.efetch(db="protein", id=gi, rettype="acc")
+                                                tmp = Entrez.efetch(db="protein", id=acc, rettype="fasta", retmode="text")
+                                                if len(tmp.read().split("\n", 1)[1]) > length:
+                                                        seq = Entrez.efetch(db="protein", id=acc, rettype="fasta", retmode="text")
+                                else:
+                                        seq = Entrez.efetch(db="protein", id=hit_id, rettype="fasta", retmode="text")
                         print(seq.read(), file=f, end='')
+
+f = open("protein_mapping.txt","w")
+f.write(str(map_dict))
+f.close()
